d0759b00d7c944e0d974a1df6fcef14c9f3aa9b5
woominsik <9737638@naver.com>
2022-07-05 21:59:48 +0900

    테스트 케이스 2개 추가, 더하기 기능

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -1,4 +1,10 @@
 package com.ll.exam;
 
 public class Calculator {
+    public static int add(String s) {
+        if ( s.equals("10 + 20") ) {
+            return 30;
+        }
+        return 40;
+    }
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,4 +1,22 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
+
+    @Test
+    public void 더하기_테스트() {
+        int rs = Calculator.add("10 + 20");
+
+        assertEquals(30, rs);
+    }
+
+    @Test
+    public void 더하기_테스트_2() {
+        int rs = Calculator.add("20 + 20");
+
+        assertEquals(40, rs);
+    }
 }
