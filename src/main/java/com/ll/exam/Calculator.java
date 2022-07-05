0ad828bcd3dabf643a532b3e6fc9ea9a318b993c
woominsik <9737638@naver.com>
2022-07-05 23:11:07 +0900

    나누기 기능 추가 및 사칙연산 기능 추가

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -1,37 +1,81 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Calculator {
     public static int calc(String s) {
-        boolean isMinus = s.indexOf(" - ") != -1;
-        boolean isMulti = s.indexOf(" * ") != -1;
 
-        if (isMinus) {
-            return calcMinus(s);
-        }else if (isMulti) {
-            return calcMulti(s);
+        s=s.replaceAll("\\s+","");
+
+        String numStr = s.replaceAll("[^0-9]", " ");
+        String opStr = s.replaceAll("[0-9]", " ");
+        StringTokenizer st = new StringTokenizer(numStr);
+
+        ArrayList<Integer>nums = new ArrayList<>();
+        while(st.hasMoreTokens()){
+            nums.add(Integer.parseInt(st.nextToken()));
         }
-        return calcPlus(s);
-    }
-    private static int calcMulti(String s) {
-        String[] sBits = s.split(" \\* ");
-        int no1 = Integer.parseInt(sBits[0]);
-        int no2 = Integer.parseInt(sBits[1]);
 
-        return no1 * no2;
-    }
-    private static int calcPlus(String s) {
-        String[] sBits = s.split(" \\+ ");
-        int no1 = Integer.parseInt(sBits[0]);
-        int no2 = Integer.parseInt(sBits[1]);
+        ArrayList<Character> ops = new ArrayList<>();
+        st = new StringTokenizer(opStr);
 
+        while(st.hasMoreTokens()){
+            ops.add(st.nextToken().charAt(0));
+        }
+
+        int num=0;
+        while(!ops.isEmpty()){
+            boolean isMul = ops.contains('*');
+            boolean isDiv = ops.contains('/');
+            int idx=0;
+            if(isMul || isDiv){
+                int m = ops.indexOf('*');
+                int d = ops.indexOf('/');
+//                System.out.println(m+" "+d);
+                if(m!=-1&&d!=-1){
+                    idx = m>d?d:m;
+                }
+                else{
+                    idx = m==-1?d:m;
+                }
+            }
+            char c= ops.get(idx);
+            int n1=nums.get(idx);
+            int n2 = nums.get(idx+1);
+            int temp=0;
+            switch (c){
+                case '+':temp+=calcPlus(n1,n2);
+                    break;
+                case '-':temp+=calcMinus(n1,n2);
+                    break;
+                case '/':temp+=calcDiv(n1,n2);
+                    break;
+                case '*':temp+=calcMul(n1,n2);
+                    break;
+            }
+            nums.remove(idx);
+            nums.remove(idx);
+            nums.add(idx,temp);
+            ops.remove(idx);
+        }
+        return nums.get(0);
+    }
+
+    private static int calcDiv(int no1, int no2) {
+        return no1/no2;
+    }
+
+    private static int calcMul(int no1, int no2) {
+        return no1*no2;
+    }
+
+    private static int calcPlus(int no1, int no2) {
         return no1 + no2;
     }
 
-    private static int calcMinus(String s) {
-        String[] sBits = s.split(" \\- ");
-        int no1 = Integer.parseInt(sBits[0]);
-        int no2 = Integer.parseInt(sBits[1]);
-
+    private static int calcMinus(int no1, int no2) {
         return no1 - no2;
     }
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,27 +1,27 @@
 package com.ll.exam;
 
+import com.ll.exam.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-
     @Test
-    public void 더하기_테스트() {
+    public void 더하기() {
         int rs = Calculator.calc("10 + 20");
 
         assertEquals(30, rs);
     }
 
     @Test
-    public void 더하기_테스트_2() {
+    public void 더하기_2() {
         int rs = Calculator.calc("20 + 20");
 
         assertEquals(40, rs);
     }
 
     @Test
-    public void 더하기_테스트_3() {
+    public void 더하기_3() {
         int rs = Calculator.calc("10 + 10");
 
         assertEquals(20, rs);
@@ -33,10 +33,37 @@ public class AppTest {
 
         assertEquals(0, rs);
     }
+
+    @Test
+    public void 나누기() {
+        int rs = Calculator.calc("10 / 10");
+
+        assertEquals(1, rs);
+    }
     @Test
     public void 곱하기() {
         int rs = Calculator.calc("10 * 10");
 
         assertEquals(100, rs);
+    }
+    @Test
+    public void 사칙연산(){
+        int rs = Calculator.calc("10 + 10+10");
+        assertEquals(30,rs);
+    }
+    @Test
+    public void 사칙연산2(){
+        int rs = Calculator.calc("10 - 10+10");
+        assertEquals(10,rs);
+    }
+    @Test
+    public void 사칙연산3(){
+        int rs = Calculator.calc("10 + 10*10");
+        assertEquals(110,rs);
+    }
+    @Test
+    public void 사칙연산4(){
+        int rs = Calculator.calc("10 / 10*10");
+        assertEquals(10,rs);
     }
 }
